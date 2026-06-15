014a7c15bd159b23cd765a798e7bb4f65ea52ad3
skarlalu <145408766+skarlalu@users.noreply.github.com>
2026-06-14 23:32:49 -0500

    Add files via upload

--- programacion_poo/main.py
+++ programacion_poo/main.py
@@ -1,1 +1,17 @@
+from mascota import Mascota
 
+
+if __name__ == "__main__":
+    print("--- Gestión de Mascota (Programación Orientada a Objetos) ---\n")
+    
+    mascota1 = Mascota("Locky", "Perro", 1)
+    mascota2 = Mascota("Elis", "Gato", 2)
+
+    print("Mascota 1:")
+    mascota1.mostrar_informacion()
+    mascota1.hacer_sonido()
+    print("-" * 40)
+    
+    print("Mascota 2:")
+    mascota2.mostrar_informacion()
+    mascota2.hacer_sonido()
